f53bc58cb9f69610283ed3280e52353f643e39aa
Panzho0 <52612917+Panzho0@users.noreply.github.com>
2021-09-25 10:57:36 -0300

    vista postulacion, create account
    se modifica la vista views/createAccount, se crea vista con formulario en view/RealizedPostulation

--- src/views/CreateAccount.vue
+++ src/views/CreateAccount.vue
@@ -1,93 +1,105 @@
 <template>
-    <div class="flex flex-col w-full lg:flex-row">
-        <div class="grid flex-grow card rounded-box place-items-center">
-            <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                <div class="card-body">
-                    <p class="text-center text-2xl">Crear Cuenta</p>
-                    <div class="form-control">
-                        <label class="label">
-                            <span class="label-text">Nombre</span>
-                        </label>
-                        <input
-                            id="thename"
-                            type="text"
-                            placeholder=""
-                            class="input input-bordered"
-                        />
-                    </div>
-                    <div class="form-control">
-                        <label class="label">
-                            <span class="label-text">Apellido</span>
-                        </label>
-                        <input id="lastname" type="text" placeholder="******" class="input input-bordered" />
-                    </div>
-                    <div class="form-control">
-                        <label class="label">
-                            <span class="label-text">Rut</span>
-                        </label>
-                        <input id="rut" type="text" placeholder="11.111.111-1" class="input input-bordered" />
-                    </div>
-                    <div class="form-control">
-                        <label class="label">
-                            <span class="label-text">Correo Electrónico</span>
-                        </label>
-                        <input
-                            id="email"
-                            type="text"
-                            placeholder="ejemplo@gmail.com"
-                            class="input input-bordered"
-                        />
-                    </div>
-                    <div class="form-control">
-                        <label class="label">
-                            <span class="label-text">Teléfono/Celular</span>
-                        </label>
-                        <input id="phone" type="text" placeholder="12345678" class="input input-bordered" />
-                    </div>
-                    <div class="form-control">
-                        <label class="label">
-                            <span class="label-text">Contraseña</span>
-                        </label>
-                        <input id="pass" type="text" placeholder="******" class="input input-bordered" />
-                    </div>
-                    <div class="form-control">
-                        <label class="label">
-                            <span class="label-text">Confirmar Contraseña</span>
-                        </label>
-                        <input type="text" placeholder="******" class="input input-bordered" />
-                    </div>
-                    <div class="form-control">
-                        <input @click="createAccount()" type="button" value="Registrarse" class="btn btn-neutral mt-3" />
-                    </div>
-                </div>
-            </div>
+  <div class="grid grid-cols-1 md:grid-cols-3 content-center text-center p-3">
+    <div></div>
+    <div>
+      <h1 class="my-3 text-4xl font-bold">Postular taller</h1>
+      <form class="mt-12">
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Nombre</span>
+          </label>
+          <input
+            type="text"
+            class="input input-bordered"
+            id="thename"
+            required
+          />
+
+          <label class="label">
+            <span class="label-text">Apellido</span>
+          </label>
+          <input
+            type="text"
+            class="input input-bordered"
+            id="lastname"
+            required
+          />
+
+          <label class="label">
+            <span class="label-text">Rut</span>
+          </label>
+          <input type="text" class="input input-bordered" id="rut" required />
+
+          <label class="label">
+            <span class="label-text">Correo electrónico</span>
+          </label>
+          <input type="text" class="input input-bordered" id="email" required />
+
+          <label class="label">
+            <span class="label-text">Telefono</span>
+          </label>
+          <input type="text" class="input input-bordered" id="phone" required />
+
+          <label class="label">
+            <span class="label-text">Contraseña</span>
+          </label>
+          <input
+            type="password"
+            class="input input-bordered"
+            id="pass"
+            required
+          />
+
+          <label class="label">
+            <span class="label-text">Confirmar contraseña</span>
+          </label>
+          <input
+            type="password"
+            class="input input-bordered"
+            id="pass2"
+            required
+          />
+
+          <button
+            @click="createAccount()"
+            class="w-full px-8 py-3 rounded-md btn text-coolGray-50 mt-5"
+          >
+            Registrarse
+          </button>
         </div>
+      </form>
     </div>
+    <div></div>
+  </div>
 </template>
+
 <script setup>
-import { useRouter } from 'vue-router'
-import axios from 'axios'
-const router = useRouter()
+import { useRouter } from "vue-router";
+import axios from "axios";
+const router = useRouter();
 const createAccount = () => {
-    axios.post('http://localhost:8080/CreateAccount', {
-        headers: {
-            "Content-type": "application/json"
-        },
-        data: {
-            user_rut: rut.value,
-            user_type_id: 2,
-            user_name: thename.value,
-            user_last_name: lastname.value,
-            user_email: email.value,
-            user_phone: phone.value,
-            user_password: pass.value,
-            user_status: 'enabled'
-        }
-    }).then(function (response) {
-        console.log(response)
-        router.push('/Login')
-    }).catch(function (error) {
-        console.log(error)
+  axios
+    .post("http://localhost:8080/CreateAccount", {
+      headers: {
+        "Content-type": "application/json",
+      },
+      data: {
+        user_rut: rut.value,
+        user_type_id: 2,
+        user_name: thename.value,
+        user_last_name: lastname.value,
+        user_email: email.value,
+        user_phone: phone.value,
+        user_password: pass.value,
+        user_status: "enabled",
+      },
     })
-}
+    .then(function (response) {
+      console.log(response);
+      router.push("/Login");
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
 </script>
