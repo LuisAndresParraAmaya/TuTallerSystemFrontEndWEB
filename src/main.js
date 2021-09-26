5aaba48916797db487421635841cc019fed80ea6
LuisAndresParraAmaya <LuisAndresParraAmaya@gmail.com>
2021-09-25 22:52:07 -0300

    Reestablecer contraseña parte 1

--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import { router } from './router.js'
-createApp(App).use(router).mount('#app')
+// Creating App
+const app = createApp(App)
+// Adding Plugins To App
+app.use(router)
+// Mounting App
+app.mount('#app')
