cc0a6c0d0609a9cf2c8f33af095f35328cb43035
Panzho0 <52612917+Panzho0@users.noreply.github.com>
2021-09-26 23:01:07 -0300

    Crear vista rechazo de taller
    se creo vista rechazo de taller

---
+++ src/view/RefuseWorkshop/Template.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-3 content-center text-center p-3">
+    <div></div>
+    <div>
+      <h1 class="my-3 text-4xl font-bold">
+          Rechazo de Taller
+      </h1>
+      <form @submit.prevent class="mt-12">
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Razon de rechazo </span>
+          </label>
+          <textarea style="resize:none"
+            class="input-bordered textarea"
+            name="descriptionArea"
+            id="postulation_message"
+            cols="30"
+            rows="5"
+          ></textarea>
+          <button
+            v-on:click="sendCode"
+            class="w-full px-8 py-3 rounded-md btn text-coolGray-50 mt-5"
+          >
+            Rechazar
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
