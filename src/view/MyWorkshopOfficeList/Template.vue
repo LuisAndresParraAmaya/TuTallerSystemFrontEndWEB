31f7887b925b40fa41ab45e80b341ac59a250e61
Panzho0 <52612917+Panzho0@users.noreply.github.com>
2021-10-06 19:11:30 -0300

    se crea vista de lista de sucursales de taller

---
+++ src/view/MyWorkshopOfficeList/Template.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="grid place-items-center mt-12">
+    <h1 class="my-3 text-4xl font-bold text-center mb-12">Sucursales de los Tuercas</h1>
+    <div class="rounded-xl border shadow-2xl">
+      <table class="table-auto ">
+        <thead class="text-center">
+          <tr>
+            <th class="p-6">Región</th>
+            <th class="p-6">Comuna</th>
+            <th class="p-6">Dirección</th>
+            <th class="p-6">Número de teléfono</th>
+            <th class="p-6">Estado suscripción</th>
+            <th class="p-6"></th>
+          </tr>
+        </thead>
+        <tbody class="divide-y text-center">
+          <tr class="divide-x">
+            <td class="p-6">Región Metropolitana</td>
+            <td class="p-6">Santiago</td>
+            <td class="p-6">av diez de julio 5526</td>
+            <td class="p-6">2258457896</td>
+            <td class="p-6">suscrito</td>
+            <td class="p-6">
+              <button class="btn btn-success rounded-full w-24">Ver Más</button>
+            </td>
+          </tr>
+           
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
