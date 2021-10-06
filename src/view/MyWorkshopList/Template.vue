b9bc4afd00a844e08018d4ae1c2cbfe5cde6941e
Panzho0 <52612917+Panzho0@users.noreply.github.com>
2021-10-06 19:10:32 -0300

    se creo vista de lista mis taller
    se creo vista de lista mis taller

---
+++ src/view/MyWorkshopList/Template.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="grid grid-cols-1 px-16">
+    <h1 class="my-3 text-4xl font-bold text-center">Lista de mis talleres</h1>
+    <div class="table text-center items-center">
+      <table class="w-full">
+        <thead class="">
+          <tr class="border-black">
+            <th>Nombre de taller</th>
+            <th>Número de teléfono</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Los tuercas</td>
+            <td>2258457896</td>
+            <td>
+              <button class="btn btn-success rounded-full">Ver Más</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
