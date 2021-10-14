bc1732e4484310f162355ca58d76c0431168b2f2
Panzho0 <52612917+Panzho0@users.noreply.github.com>
2021-10-13 22:00:26 -0300

    Creacion vista Realizar reclamo
    se crea la vista para realizar reclamo

--- src/view/ReclaimWorkshop/Template.vue
+++ src/view/ReclaimWorkshop/Template.vue
@@ -1,9 +1,43 @@
+hola
 <template>
-    <div>
-    <h1>Hola Bro</h1>
+
+  <div class="grid grid-cols-1 lg:grid-cols-4 p-1 place-items-center">
+    <div class="lg:col-start-2 lg:col-span-2 lg:w-3/4 p-2">
+    
+      <h1 class="my-3 text-4xl font-bold text-center mb-20 mt-12">
+        Realizar Reclamo
+      </h1>
+      <form @submit.prevent>        
+          <div>
+            <label class="label">Reclamo:</label>
+            <textarea
+              id="reclaimDescription"
+              class="px-3
+              py-3
+              placeholder-gray-400
+              text-gray-600
+              relative
+              bg-white
+              rounded
+              text-sm
+              border border-gray-400
+              outline-none
+              focus:outline-none
+              focus:ring
+              w-full"
+              cols="30"
+              rows="5"
+              style="resize: none"
+            ></textarea>
+          </div>
+          <div class="flex place-content-between">
+            <button
+              class="w-full py-2 rounded-md bg-green-600 hover:bg-green-700 text-coolGray-50 mt-5"
+            >
+              Enviar reclamo
+            </button>
+          </div>        
+      </form>
     </div>
+  </div>  
 </template>
-
-<style src="./Style.css"></style>
-<script src="./Component.js"></script>
-
