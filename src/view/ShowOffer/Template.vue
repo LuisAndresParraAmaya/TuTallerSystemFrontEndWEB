03f4832416dca0cdc47eba938403ecb2671f3f7a
Panzho0 <52612917+Panzho0@users.noreply.github.com>
2021-12-06 16:14:48 -0300

    edicion
    se agrega traduccion al tipo de plan y periodicidadad

--- src/view/ShowOffer/Template.vue
+++ src/view/ShowOffer/Template.vue
@@ -2,6 +2,7 @@
   <div style="display: flex; justify-content: center">
     <div class="border mockup-window bg-base-300">
       <h1 class="my-3 text-4xl font-bold text-center mb-12">Oferta</h1>
+      
       <div class="grid grid-cols-1 xl:grid-cols-1 p-1">
         <div class="rounded-xl shadow-2xl w-full" v-for="o in ofertas" :key="o">
           <label class="label"
@@ -24,7 +25,7 @@
             </label>
 
             <ul v-for="a in servicios" :key="a">
-              <li v-if="a.offer_id === o.id">{{ a.nombreplan }}</li>
+              <li v-if="a.offer_id === o.id">{{ traductor(a.nombreplan) }}</li>
             </ul>
           </div>
           <div v-if="datobutton === 'workshopOfficeService'">
