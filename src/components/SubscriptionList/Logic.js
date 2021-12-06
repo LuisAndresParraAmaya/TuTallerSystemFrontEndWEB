8fc7c74fcbde4f3fe0343c3404a920c0ec00b88c
Panzho0 <52612917+Panzho0@users.noreply.github.com>
2021-12-06 16:28:43 -0300

    edicion vista y logica
    se edita la logica para validar la informacion que envia el boton ver mas

--- src/components/SubscriptionList/Logic.js
+++ src/components/SubscriptionList/Logic.js
@@ -2,6 +2,11 @@ import axios from 'axios'
 import { subscriptionNameEnglishToSpanish } from '../../utils/translations'
 
 export default {
+    data(){
+        return{
+            suscripcion:[]
+        }
+    },
     async setup() {
        
         let subs = await axios.get("http://localhost:8080/SubscriptionList", {
@@ -13,5 +18,17 @@ export default {
             console.log(error)
         })
         return {subs, subscriptionNameEnglishToSpanish};
+    },
+    methods:{
+        verMas(precio, peirodicidad, descripcion, nombre){
+            this.suscripcion.push({
+                precios:precio,
+                periodos:peirodicidad,
+                descripciones:descripcion,
+                nombre:nombre
+            })
+            localStorage.setItem('suscripcionplan', JSON.stringify(this.suscripcion))
+            this.$router.push('Subscription')
+        }
     }
 }
--- src/components/SubscriptionList/Template.vue
+++ src/components/SubscriptionList/Template.vue
@@ -6,17 +6,45 @@
           <th class="py-3 px-2">Nombre</th>
           <th class="py-3 px-2">Periodicidad</th>
           <th class="py-3 px-2">Precio</th>
-          <th class="py-3 px-2">Oferta</th>          
+          <th class="py-3 px-2">Oferta</th>
+          <th class="py-3 px-2">Ver mas</th>
         </tr>
       </thead>
       <tbody class="divide-y text-center">
-     
         <tr v-for="sub in subs" :key="sub" class="divide-x">
-          <td class="py-3 px-2">{{subscriptionNameEnglishToSpanish(sub.name)}}</td>
-          <td class="py-3 px-2">{{subscriptionNameEnglishToSpanish(sub.periodicity)}}</td>
-          <td class="py-3 px-2"><p v-if="sub.offer_discount>0"><strike>${{sub.price}} CLP</strike></p> <p v-else>${{sub.price}} CLP</p></td>
-          <td class="py-3 px-2"><p v-if="sub.offer_discount>0">${{sub.offer_price}} CLP</p><p v-else>Sin oferta</p></td>
-          
+          <td class="py-3 px-2">
+            {{ subscriptionNameEnglishToSpanish(sub.name) }}
+          </td>
+          <td class="py-3 px-2">
+            {{ subscriptionNameEnglishToSpanish(sub.periodicity) }}
+          </td>
+          <td class="py-3 px-2">
+            <p v-if="sub.offer_discount > 0">
+              <strike>${{ sub.price }} CLP</strike>
+            </p>
+            <p v-else>${{ sub.price }} CLP</p>
+          </td>
+          <td class="py-3 px-2">
+            <p v-if="sub.offer_discount > 0">${{ sub.offer_price }} CLP</p>
+            <p v-else>Sin oferta</p>
+          </td>
+          <td>
+            <button
+              id="AddOffer"
+              v-on:click="
+                verMas(
+                  sub.price,
+                  sub.periodicity,
+                  sub.description,
+                  sub.name
+
+                )
+              "
+              class="w-auto px-1 py-1 m-2 rounded-md btn text-coolGray-50"
+            >
+              Ver mas
+            </button>
+          </td>
         </tr>
       </tbody>
     </table>
