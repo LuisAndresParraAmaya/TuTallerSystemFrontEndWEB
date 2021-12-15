4cc01a80f1016435d7dece9b36ecdb0d63a2b68f
Panzho0 <52612917+Panzho0@users.noreply.github.com>
2021-12-15 12:48:37 -0300

    edicion
    se edita en la logica algunas funciones para redireccionar y en la vista se agrega vista para tenico al finalizar proceso

--- src/view/WorkshopOfficeWork/Logic.js
+++ src/view/WorkshopOfficeWork/Logic.js
@@ -21,9 +21,7 @@ export default {
     created() {
        
     },
-    mounted() {
-
-        
+    beforeMount(){
         this.status= sessionStorage.getItem('status')
         this.officeWorkId = sessionStorage.getItem('workshopofficeid')
         this.nombreservicio= sessionStorage.getItem('nombreservicio')
@@ -36,6 +34,11 @@ export default {
         this.bd()
 
         }
+    },
+    mounted() {
+
+        
+        
 
     },
     methods: {
@@ -53,7 +56,7 @@ export default {
             let a = confirm('¿Estas seguro de informar el retiro del vehículo?\n Procura que el trabajo en el vehículo esté terminado antes de informar al cliente que ya puede venir a retirarlo')
             if (a) {
                 this.bdComplete(id)
-                location.reload()
+                this.$router.go()
             }
 
         },
@@ -61,7 +64,7 @@ export default {
             let a = confirm('¿Estas seguro de confirmar que el cliente ha retIrado su vehículo?')
             if (a) {
                 this.bdComplete(id)
-                location.reload()
+                this.$router.go()
             }
         },
         confirmFin() {
@@ -144,7 +147,7 @@ export default {
             if (r == true) {
                 console.log(milestoneid)
                 this.bdComplete(milestoneid)
-                location.reload()
+                this.$router.go()
             }
         },
         index() {
