4d988f84966af5e7326807ddd167024b3ea867c0
LuisAndresParraAmaya <LuisAndresParraAmaya@gmail.com>
2021-12-19 12:17:29 -0300

    CAMBIOS DE TRANSBANK

---
+++ src/view/RealizePay/PaymentReject/Template.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1 class="my-3 text-4xl font-bold">Operación Fallida</h1>
+        <h1>tu pago no fue recibido, intenta pagar nuevamente.</h1>
+    </div>
+</template>
